c9f8c34e5e8d6c6588ddad3d1340e621a671137f
nicolitoo <nastros3@outlook.com>
2024-09-28 17:42:09 -0400

    added MapsView page for UI

---
+++ AidAlert/MapsView.swift
@@ -0,0 +1,18 @@
+//
+//  MapsView.swift
+//  AidAlert
+//
+//  Created by Nicolas Astros on 9/28/24.
+//
+
+import SwiftUI
+
+struct MapsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapsView()
+}
