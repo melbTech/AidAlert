e85f2e5a20c4b14320bb327bfb722dc4019d4d20
Melvin Santos <itsmelvinsantos@gmail.com>
2024-09-28 15:10:39 -0400

    Initial Commit

---
+++ AidAlert/AidAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  AidAlertApp.swift
+//  AidAlert
+//
+//  Created by Melvin Santos on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct AidAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AidAlertTests/AidAlertTests.swift
@@ -0,0 +1,17 @@
+//
+//  AidAlertTests.swift
+//  AidAlertTests
+//
+//  Created by Melvin Santos on 9/28/24.
+//
+
+import Testing
+@testable import AidAlert
+
+struct AidAlertTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
