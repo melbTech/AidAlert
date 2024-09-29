113ee20dcbbcc98ba86e5a959c940235aeb6a7a5
Miguel Castro <mcastro32902@gmail.com>
2024-09-29 09:06:48 -0400

    Menu

--- AidAlert/MoreScreen.swift
+++ AidAlert/MoreScreen.swift
@@ -9,8 +9,101 @@ import SwiftUI
 
 struct MoreScreen: View {
     var body: some View {
-        Text("More Screen")
-            .font(.largeTitle)
+        NavigationView {
+            List {
+                NavigationLink(destination: ContactsView()) {
+                    HStack {
+                        Image(systemName: "person.2.fill")
+                            .foregroundColor(.blue)
+                        Text("Contacts")
+                            .font(.headline)
+                    }
+                }
+                
+                NavigationLink(destination: DonateView()) {
+                    HStack {
+                        Image(systemName: "heart.fill")
+                            .foregroundColor(.red)
+                        Text("Donate")
+                            .font(.headline)
+                    }
+                }
+                
+                NavigationLink(destination: VolunteerView()) {
+                    HStack {
+                        Image(systemName: "hands.sparkles.fill")
+                            .foregroundColor(.green)
+                        Text("Volunteer")
+                            .font(.headline)
+                    }
+                }
+                
+                NavigationLink(destination: ProvideShelterView()) {
+                    HStack {
+                        Image(systemName: "house.fill")
+                            .foregroundColor(.orange)
+                        Text("Provide Shelter")
+                            .font(.headline)
+                    }
+                }
+                
+                NavigationLink(destination: FAQView()) {
+                    HStack {
+                        Image(systemName: "questionmark.circle.fill")
+                            .foregroundColor(.purple)
+                        Text("FAQ")
+                            .font(.headline)
+                    }
+                }
+            }
+            .navigationTitle("More Options")
+        }
+    }
+}
+
+// Placeholder views for each section
+struct ContactsView: View {
+    var body: some View {
+        Text("Contacts")
+            .font(.title)
             .padding()
+    }
+}
+
+struct DonateView: View {
+    var body: some View {
+        Text("Donate")
+            .font(.title)
+            .padding()
+    }
+}
+
+struct VolunteerView: View {
+    var body: some View {
+        Text("Volunteer")
+            .font(.title)
+            .padding()
+    }
+}
+
+struct ProvideShelterView: View {
+    var body: some View {
+        Text("Provide Shelter")
+            .font(.title)
+            .padding()
+    }
+}
+
+struct FAQView: View {
+    var body: some View {
+        Text("FAQ")
+            .font(.title)
+            .padding()
+    }
+}
+
+struct MoreScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MoreScreen()
     }
 }
